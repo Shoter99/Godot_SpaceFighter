0955284609eef0438409390afea2d8ea6b1a0105
Vivi <shoter998@gmail.com>
2022-03-27 15:59:18 +0200

    added enemy following player, player shooting

--- Actors/Enemy.gd
+++ Actors/Enemy.gd
@@ -1,28 +1,39 @@
 extends KinematicBody2D
 
 
-var player = null
+onready var player = $"/root/Node2D/Player"
 var move = Vector2.ZERO
 export var speed = 1000.0
 var move_rot = Vector2.ZERO
+var following = true
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
 
 func _process(delta):
 	move = Vector2.ZERO
 	
-	if player != null:
+	if following:
 		move = position.direction_to(player.position) * speed	* delta
+		move = move.normalized()
+		move_rot = move_and_slide(move)
+		move = move_and_collide(move)
+		rotation = lerp_angle(rotation, move_rot.angle() + PI/2 - PI/2, 20.0 * delta)
 	else:
 		move = Vector2.ZERO
+		var dir = player.position - position
+		rotation = dir.angle()
+		
+		
 	
-	move = move.normalized()
-	move_rot = move_and_slide(move)
-	move = move_and_collide(move)
-	rotation = lerp_angle(rotation, move_rot.angle() + PI/2 - PI/2, 20.0 * delta)
+	
+
+
 
 func _on_Area2D_body_entered(body):
-	if body != self:
-		player = body
+	if body == player:
+		following = false
 		
+
+
+func _on_Area2D_body_exited(body):
+	if body == player:
+		following = true
--- Actors/Player.gd
+++ Actors/Player.gd
@@ -2,6 +2,11 @@ extends Actor
 
 export var max_speed = 1000.0
 export var drag = 4.0
+export var bullet_speed = 1000.0
+export var fire_rate = .2
+
+var bullet = preload("res://Actors/Bullet.tscn")
+var can_fire = true
 
 var _velocity = Vector2.ZERO
 
@@ -15,4 +20,16 @@ func _process(delta):
 	direction = direction.normalized()
 	_velocity = CalcVel(_velocity, drag, direction, max_speed)
 	_velocity = move_and_slide(_velocity)
-	rotation = lerp_angle(rotation, _velocity.angle() + PI/2 - PI/2, 20.0 * delta)
+	#rotation = lerp_angle(rotation, _velocity.angle() + PI/2 - PI/2, 20.0 * delta)
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_pressed("Shoot") and can_fire:
+		var bullet_instance = bullet.instance()
+		bullet_instance.position = get_global_position()
+		bullet_instance.rotation_degrees = rotation_degrees
+		bullet_instance.apply_impulse(Vector2.ZERO, Vector2(bullet_speed,0).rotated(rotation))
+		get_tree().get_root().add_child(bullet_instance)
+		
+		can_fire = false
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		can_fire = true
